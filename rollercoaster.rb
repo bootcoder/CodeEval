725628f44df1949aeab08477f678c02deb898cf6
bootcoder <bootcoder@gmail.com>
2016-06-11 03:08:51 -0700

    refactored solution

--- rollercoaster.rb
+++ rollercoaster.rb
@@ -28,59 +28,16 @@
 ### Initial    Solution ##
 ##########################
 
-require 'pry-byebug'
+def cycle_char(char)
+  @upCaseIt = !@upCaseIt
+  @upCaseIt ? char.upcase : char.downcase
+end
 
 File.open('input_sample.txt').each_line do |line|
 # File.open(ARGV[0]).each_line do |line|
-line.chomp!
-result = ""
-upCaseIt = true
-thing = line.chars.map! do |char|
-  if upCaseIt && ('a'..'z').to_a.include?(char.downcase)
-    upCaseIt = false
-    char.upcase
-  elsif !upCaseIt && ('a'..'z').to_a.include?(char.downcase)
-    upCaseIt = true
-    char.downcase
-  else
-    char
+  @upCaseIt = false
+  result = line.chomp.chars.map do |char|
+    ('a'..'z').to_a.include?(char.downcase) ? cycle_char(char) : char
   end
+  puts result.join("")
 end
-
-# line.each_char do |char|
-
-# end
-# binding.pry
-p thing.join("")
-end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
